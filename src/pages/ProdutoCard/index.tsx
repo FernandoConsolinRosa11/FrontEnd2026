2333e9e4ac0e1dafa0f81ead67c7e227a135ee06
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-04-04 12:35:21 -0300

    ExplorarCardRoutes

---
+++ src/pages/ProdutoCard/index.tsx
@@ -0,0 +1,7 @@
+export default function ProdutoCard(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home, Login, Register , ErrorPage, Explorar, Garagem} from "../pages";
 import Menu from "../layout/Menu";
 import UserProfile from "../pages/UserProfile";
+import ProdutoCard from "../pages/ProdutoCard";
 
 export default function AppRoutes() {
     return(
@@ -12,6 +13,7 @@ export default function AppRoutes() {
             <Route path="/Register" element={<Register/>} />
             <Route path="/Menu" element={<Menu/>} />
             <Route path="/Explorar" element={<Explorar/>} />
+            <Route path="/Explorar/:id" element={<ProdutoCard/>} />
             <Route path="/Garagem/:id" element={<Garagem/>} />
             <Route path="/Perfil/:id" element={<UserProfile/>} />
         </Routes>
