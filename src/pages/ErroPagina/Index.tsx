60ffb33e79d40ee53facc2e83f062b026ffc092d
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-04-13 21:28:39 -0300

    Update Index.tsx

--- src/pages/ErroPagina/Index.tsx
+++ src/pages/ErroPagina/Index.tsx
@@ -5,17 +5,19 @@ export default function ErrorPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-[#121212] text-white py-20! text-center">
-      <div className="text-4xl my-3 text-[#C59958]!">Erro 404</div>
-      <div className="my-3">
-        A pagina que está procurando não existe!
+    <div className="bg-[#121212] text-white py-20! text-center min-h-screen flex flex-col items-center justify-center">
+      <div className="scale-150">
+        <div className="text-4xl my-3 text-[#C59958]!">Erro 404</div>
+        <div className="my-3">
+          A pagina que está procurando não existe!
+        </div>
+        <Button
+          texto=" ← Retornar"
+          onClick={() => {
+            navigate('/');
+          }}
+        />
       </div>
-      <Button
-        texto=" ← Retornar"
-        onClick={() => {
-          navigate('/');
-        }}
-      />
     </div>
   );
 }
